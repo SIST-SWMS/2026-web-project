eadf2cfa80862be1b0be4e54bd5c0f0201b4f69b
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-07-20 16:13:12 +0900

    이윤호 7/20 QnaVO,DAO,Model,Mapper 등록

---
+++ SWMS/src/main/java/com/sist/dao/QnaDAO.java
@@ -0,0 +1,35 @@
+package com.sist.dao;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.sist.commons.*;
+import com.sist.vo.*;
+public class QnaDAO {
+    private static SqlSessionFactory ssf;
+    static
+    {
+    	ssf=CreateSqlSessionFactory.getSsf();
+    }
+    
+    /*
+     *  <select id="qnaListData" resultType="QnaVO" parameterType="string">
+		   SELECT q.goods_no,
+		          g.goods_name,
+		          g.poster_url
+		   FROM goods g
+		   JOIN qna q ON g.goods_no=q.goods_no
+		   WHERE q.id=#{id}
+		   ORDER BY q.qna_no DESC                   
+		  </select>
+     * 
+     */
+    public static List<QnaVO> qnaListData(String id)
+    {
+    	SqlSession session=ssf.openSession();
+    	List<QnaVO> list=session.selectList("qnaListData",id);
+    	session.close();
+    	return list;
+    }
+}
---
+++ SWMS/src/main/java/com/sist/model/QnaModel.java
@@ -0,0 +1,26 @@
+package com.sist.model;
+import java.util.*;
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+import com.sist.dao.QnaDAO;
+import com.sist.vo.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class QnaModel {
+    @RequestMapping("mypage/qna.do")
+    public String qna(HttpServletRequest request, HttpServletResponse response) 
+    {
+    	int goods_no=Integer.parseInt(request.getParameter("goods_no"));
+    	
+    	QnaVO vo=QnaDAO.qnaListData(null);
+    	List<QnaVO> list=new ArrayList<QnaVO>();
+    	list.add(vo);
+    	request.setAttribute("list", list);
+    	 request.setAttribute("mypage_content", "../mypage/qna.jsp");
+ 	    request.setAttribute("main_jsp", "../mypage/mypage.jsp");
+ 	    return "../main/main.jsp";
+    }
+    
+}
